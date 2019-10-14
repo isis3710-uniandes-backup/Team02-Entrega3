98cdb0c19163c35a4b1ab95383d821d8b36f8052
José Darío Flórez <jd.florezg1@uniandes.edu.co>
2019-10-14 17:04:58 -0500

    fix router

--- imports/ui/routes.js
+++ imports/ui/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import{ HashRouter, Route, Link} from 'react-router-dom';
+import{ Router, Route, Link} from 'react-router-dom';
 
 //Componentes a Enrutar.
 
@@ -8,21 +8,23 @@ import OfertasList from './OfertasList';
 import Login from './Login';
 import Inicio from './Inicio';
 import UsuarioOfertas from './UsuarioOfertas';
+import history from 'history'
 
+const browserHistory = history.createBrowserHistory();
 /**
- * Rutas del front(esta login y ofertas por ahora)
+ * Rutas del front
  */
 
 //TODO Arreglar el router para que no se solapen los componentes.
 
 Meteor.startup(()=>{
     render(
-        <HashRouter >
-            <Route path="/" component={Inicio}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/ofertas" component={OfertasList}/>
-            <Route path="/uofertas" component={UsuarioOfertas}/>
-        </HashRouter>,
+        <Router history={browserHistory}>
+            <Route exact path="/" component={Inicio}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/ofertas" component={OfertasList}/>
+            <Route exact path="/uofertas" component={UsuarioOfertas}/>
+        </Router>,
         document.getElementById('root')
     );
 });
